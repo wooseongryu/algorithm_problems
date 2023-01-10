716da7338e263dca8f8f27ffae05584445ce1e75
wryu <useong3754@gmail.com>
2023-01-10 18:24:43 +0900

    937_reorder_log_files

---
+++ python_string_base/937_reorder_log_files.py
@@ -0,0 +1,16 @@
+def reorder_log_files(logs: list[str]) -> list[str]:
+    digits, letters = [], []
+
+    for log in logs:
+        if log.split()[1].isdigit():
+            digits.append(log)
+        else:
+            letters.append(log)
+
+    letters.sort(key=lambda x: (x.split()[1:], x.split()[0]))
+
+    return letters + digits
+
+
+logs = ["dig1 8 1 5 1", "let1 art can", "dig2 3 6", "let2 own kit dig", "let3 art zero"]
+print(reorder_log_files(logs))
